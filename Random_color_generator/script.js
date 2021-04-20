f81c0b734f24faf5919821fa0a2d8cf647981801
saisumanthkumar <74057515+saisumanthkumar@users.noreply.github.com>
2021-04-20 18:17:32 +0530

    random_color_generator

---
+++ Random_color_generator/script.js
@@ -0,0 +1,37 @@
+var list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', ''];
+
+let history = [];
+var color = '#';
+
+for (var i = 0; i < 6; i++) {
+    var code = Math.floor(Math.random() * 16);
+    color += list[code];
+}
+
+document.getElementById('presentcolor').textContent = color;
+document.body.style.backgroundColor = color;
+var his = document.getElementById('his');
+var p = document.createElement("p");
+var text = document.createTextNode(color)
+p.appendChild(text)
+p.style.backgroundColor = color;
+his.appendChild(p)
+
+function colorget() {
+    var color = '#';
+
+    for (var i = 0; i < 6; i++) {
+        var code = Math.floor(Math.random() * 16);
+        color += list[code];
+    }
+
+    document.getElementById('presentcolor').textContent = color;
+    document.body.style.backgroundColor = color;
+    var his = document.getElementById('his');
+    var p = document.createElement("p");
+    var text = document.createTextNode(color)
+    p.appendChild(text)
+    p.style.backgroundColor = color;
+    his.appendChild(p)
+
+}
